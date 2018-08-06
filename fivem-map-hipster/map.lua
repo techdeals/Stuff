086987d4b15863c5a11e416ff8987249aa0ec0b6
OfficerScentral <41249424+OfficerScentral@users.noreply.github.com>
2018-08-05 20:59:06 -0700

    iii

---
+++ fivem-map-hipster/map.lua
@@ -0,0 +1,3 @@
+spawnpoint 'a_f_m_downtown_01' { x = 2395.096, y =  3049.616, z = 60.053 }  
+--
+#Edited by Vasco
---
+++ fivem-map-skater/map.lua
@@ -0,0 +1,15 @@
+spawnpoint 'a_f_m_downtown_01' { x = 2395.096, y =  3049.616, z = 60.053 }               
+
+
+--
+
+
+
+
+
+
+
+
+#Edited by Vasco
+
+

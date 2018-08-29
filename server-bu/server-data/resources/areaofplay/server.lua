f20cc985d1afa9bdb7dcb7378cf7a5d63d25f83a
Yeet <42053129+LossRP@users.noreply.github.com>
2018-08-28 21:18:41 -0400

    5456456456456564564

---
+++ server-bu/server-data/resources/areaofplay/server.lua
@@ -0,0 +1,62 @@
+CurrentAOP = "Sandy"
+CurrentAOP2 = "Shores"
+
+local allowed =  
+{
+	"steam:110000131e96753",
+
+}		
+
+AddEventHandler('chatMessage', function(source, name, msg)
+    local identifier = GetPlayerIdentifiers(source)[1]
+    local args = stringsplit(msg, " ")
+    if args[1] == "/aop" then
+
+    if tablelength(args) > 2 then
+    CancelEvent()
+    if checkAllowed(identifier) then
+    CurrentAOP = args[2]
+    CurrentAOP2 = args[3]
+    TriggerEvent('aop:requestSync')
+    TriggerClientEvent('chatMessage', -1, '^1[LossRP]', {255, 255, 255}, " ^7AOP has been changed to ^1" .. CurrentAOP .. " " .. CurrentAOP2 .. '^7.\n Please finish your current RP and move.')
+    else
+    TriggerClientEvent('chatMessage', -1, '', {255, 255, 255}, '^7Incorrect syntax.')
+    end
+    end
+    end
+end)
+
+function checkAllowed(id) -- function checkAllowed(). id is just a returned value (forgot what the actual word is xD)
+	for k, v in pairs(allowed) do -- for repeat to go thru the allowed table. k = key and v = value
+		if id == v then -- checks if id is equal to v
+			print('Returns true')
+			return true -- returns true if id is equal to v
+		end
+	end
+	
+	return false -- in case nothing is in the array, just a fail safe so it returns false basically
+end
+
+RegisterServerEvent('aop:requestSync')
+AddEventHandler('aop:requestSync', function()
+
+    TriggerClientEvent('aop:updateAOP', -1, CurrentAOP, CurrentAOP2)
+end)
+
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
---
+++ server-bu/server-data/resources/black/carblacklist.lua
@@ -0,0 +1,138 @@
+-- CONFIG --
+
+-- Blacklisted vehicle models
+carblacklist = {
+	"RHINO",
+	"BUZZARD",
+     "SAVAGE",
+	 "VALKYRIE",
+	 "AKULA",
+	 "INSURGENT",	 
+	 "TECHNICAL",
+	 "BLIMP",
+	 "BLIMP2",
+	 "HYDRA",
+	 "LAZER",
+	 "KURUMA2",
+	 "CABLECAR",
+	 "FREIGHT",
+	 "FREIGHTCAR",
+	 "FREIGHTCONT1",
+	 "FREIGHTCONT2",
+	 "FREIGHTGRAIN",
+	 "METROTRAIN",
+	 "TANKERCAR",
+	 "VALKYRIE2",
+	 "VERLIERER2",
+	 "PENETRATOR",
+	 "PHANTOM2",
+	 "RUINER2",
+	 "RUINER3",
+	 "TECHNICAL2",
+	 "INSURGENT3",
+	 "NIGHTSHARK",
+	 "OPPRESSOR",
+	 "PHANTOM3",
+	 "TAMPA3",
+ 	 "TECHNICAL3",
+	 "BOMBUSHKA",
+	 "MOLOTOK",
+	 "NOKOTA",
+	 "PYRO",
+	 "ROGUE",
+	 "VIGILANTE",
+	 "AKULA",
+	 "CHERNOBOG",
+	 "DELUXO",
+	 "KHANJALI",
+	 "STROMBERG",
+	 "THRUSTER",
+	 "VOLATOL",
+	 "CARACARA",
+	 "STRIKEFORCE",
+	 "BLIMP3",
+	 "SCRAMJET",
+	 "OPPRESSOR2",
+	 "HAULER",
+	 "HAULER2",
+	 "DUNE4",
+	 "DUNE5",
+	 "LIMO2",
+	 "STARLING",
+	 "BALLER5",
+	 "BALLER7",
+	 "COG552",
+	 "COGNOSCENTI2",
+	 "DUKES2",
+	 "SCHAFTER5",
+	 "SCHAFTER6",
+	 "DUNE2",
+	 "DUMP",
+	 "CUTTER",
+	 "RIPLEY",
+	 "ARMYTRAILER",
+	 "ARMYTRAILER2",
+	 "TR2",
+	 "TR3",
+	 "ARMYTANKER",
+	 "DOCKTRAILER",
+     "FREIGHTTRAILER",
+     "PROPTRAILER",
+     "RAKETRAILER",
+     "TRAILERS",
+     "TRAILERS2",
+     "TRAILERS3",
+     "TRAILERSMALL",
+     "TVTRAILER",
+	 "ANNIHILATOR",
+	 "HUNTER",
+	 "CARGOPLANE",
+	 "JET",
+	 "TITAN",
+	 "HANDLER"
+
+
+
+	 
+	 
+}
+
+-- CODE --
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(1)
+
+		playerPed = GetPlayerPed(-1)
+		if playerPed then
+			checkCar(GetVehiclePedIsIn(playerPed, false))
+
+			x, y, z = table.unpack(GetEntityCoords(playerPed, true))
+			for _, blacklistedCar in pairs(carblacklist) do
+				checkCar(GetClosestVehicle(x, y, z, 100.0, GetHashKey(blacklistedCar), 70))
+			end
+		end
+	end
+end)
+
+function checkCar(car)
+	if car then
+		carModel = GetEntityModel(car)
+		carName = GetDisplayNameFromVehicleModel(carModel)
+
+		if isCarBlacklisted(carModel) then
+			_DeleteEntity(car)
+			sendForbiddenMessage("")
+		end
+	end
+end
+
+function isCarBlacklisted(model)
+	for _, blacklistedCar in pairs(carblacklist) do
+		if model == GetHashKey(blacklistedCar) then
+			return true
+		end
+	end
+
+	return false
+end

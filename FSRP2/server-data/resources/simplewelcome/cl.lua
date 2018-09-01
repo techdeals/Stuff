20d3b5e86cca039a1ad0f5891e8919b187823f76
Yeet <42053129+LossRP@users.noreply.github.com>
2018-09-01 17:46:24 -0400

    ppp

--- FSRP2/server-data/resources/simplewelcome/cl.lua
+++
@@ -1,4 +0,0 @@
-AddEventHandler("playerSpawned", function(spawn)
-    Citizen.Wait(5000)
-    TriggerEvent("chatMessage", "", { 49, 196, 157 }, "Welcome to Florida State RP.\nJoin our discord at ^1https://discord.gg/rQjsXxU")
-end)
---
+++ server-91/server-data/resources/avalfire/__resource.lua
@@ -0,0 +1,19 @@
+
+files {
+	'vehicles.meta',
+	'carvariations.meta',
+	'carcols.meta',
+	'handling.meta',
+	'avalfirecontentunlocks.meta',
+	'shop_vehicle.meta',
+	
+}
+
+-- specify data file entries to be added
+-- these entries are the same as content.xml in a DLC pack
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'avalfirecontentunlocks.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'shop_vehicle.meta'
